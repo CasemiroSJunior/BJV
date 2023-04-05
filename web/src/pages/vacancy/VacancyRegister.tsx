c355cfa51c4d23675635ed4cd32637975e0cbdaf
Casemiro <lordofgamer@outlook.com>
2023-04-05 12:25:32 -0300

    Feat: Layout Creation Vacancy Register
    Created the entire Vacancy Register Layout, installed new dependencies: 
    DayJs => Manipulate Date,
    DatePicker => Select date component to include on layout.

--- web/src/pages/vacancy/VacancyRegister.tsx
+++ web/src/pages/vacancy/VacancyRegister.tsx
@@ -1,53 +1,224 @@
-import { Button, Checkbox, Chip, Divider, Grid, InputAdornment, Paper, Skeleton, TextField, Tooltip, Typography } from "@mui/material";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { Box, Button, Checkbox, Chip, FormControl, FormControlLabel, Grid, InputAdornment, InputLabel, MenuItem, OutlinedInput, Paper, Select, Stack, Switch, TextField, Tooltip, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Layout from "../layout";
 import LayoutBottom from "../layoutBottom";
-import { Buildings, CurrencyCircleDollar, PlusCircle } from 'phosphor-react'
+import { REMUNERATION, STATUS, VACANCYTYPE } from "@/config/constants";
+import { LocalizationProvider, DesktopDatePicker  } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
 export default function VacancyRegister() {
-    const [confidentialSalary, setConfidentialSalary] = useState(false) 
-    const [confidentialName, setConfidentialName] = useState(false) 
     
+    const NEW_VACANCY = {
+        titulo : null,
+        descricao : null,
+        salario: 0,
+        tipo: VACANCYTYPE.ESTAGIO,
+        remunerado: STATUS.ATIVO,
+        confidencial_nome: STATUS.INATIVO,
+        confidencial_salario: STATUS.INATIVO,
+        status: STATUS.ATIVO,
+        empresasUsersId: 1,
+        data_inicio: null,
+        data_termino: null,
+    };
+    
+    const [newVacancy, setNewVacancy] = useState(NEW_VACANCY);
+    const [startDate, setStartDate] = useState<Dayjs | null>(dayjs(new Date()),);
+    const [finalDate, setFinalDate] = useState<Dayjs | null>(dayjs(new Date()),);
+
+    const handleConfidential = (event: { target: { name: string; value: any; }; }) =>{
+        let { name, value } = event.target;
+        let TempVacancy=({...newVacancy, [name]: value})
+        console.log(TempVacancy)
+        setNewVacancy(TempVacancy)
+    }
+
+    const handleChangeStartDate = (date:Dayjs | null)=>{
+        setStartDate(date)
+    }
+    const handleChangeFinalDate = (date:Dayjs | null)=>{
+        setFinalDate(date)
+    }
 
     return (
         <>
             <Layout />
-            <Grid container className="mb-24 justify-center mt-12">
-                <Paper className="bg-white  " >
-                    <Grid container xl>
-                        <Grid xs={8} item className="p-2">
-                            <TextField label="Titulo da Vaga" className=" w-1/2" variant="outlined" />
+                <Grid container className="mb-24 mt-12 justify-center">
+                    <Paper className="bg-white w-3/6 justify-center p-2" >
+                        <Grid container spacing={2} >
+                            <Grid sm={9} xs={12} item className="w-full">
+                                <TextField name="titulo" value={newVacancy?.titulo? newVacancy.titulo : ""} onChange={handleConfidential} label="Titulo da Vaga" className=" w-full" variant="outlined" />
+                            </Grid>
+                            <Grid sm={9} xs={12} item className="w-full">
+                                <TextField 
+                                    label="Salario da Vaga"
+                                    onChange={handleConfidential}
+                                    className=" w-full" 
+                                    variant="outlined"
+                                    name="salario"
+                                    value={newVacancy?.salario}
+                                    disabled={newVacancy?.confidencial_salario == 1? true: false}
+                                    InputProps={{
+                                        startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+                                    }}
+                                />
+                            </Grid>
+                                <Grid xs={12} sm={3} item >
+                                    <Checkbox 
+                                        checked={newVacancy?.confidencial_salario == 1? true: false} 
+                                        onChange={()=>handleConfidential({target:{name: "confidencial_salario", value: newVacancy?.confidencial_salario == 1? 0 : 1 }} )}
+                                    />
+                                    <Tooltip arrow title="Caso seja marcado, o salário será OCULTADO e NÃO PODERÁ ser alterado.">
+                                    <Chip 
+                                        label="Confidencial?" 
+                                        onClick={
+                                            ()=>handleConfidential({target:{name: "confidencial_salario", value: newVacancy?.confidencial_salario == 1? 0 : 1 }} )}
+                                        variant={newVacancy?.confidencial_salario == 1? "filled" : "outlined"}
+                                    />
+                                    </Tooltip>
+                            </Grid>
+                            <Grid item xs={12} >
+                                <TextField
+                                    id="outlined-multiline-static"
+                                    label="Descrição"
+                                    multiline
+                                    rows={8}
+                                    className="w-full"
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid xs={8} item className="p-2">
-                            <TextField 
-                                label="Salario da Vaga" 
-                                className=" w-1/2" 
-                                variant="outlined"
-                                disabled={confidentialSalary}
-                                InputProps={{
-                                    startAdornment: <InputAdornment position="start">R$</InputAdornment>,
-                                  }}
-                            />
+                        <Grid container spacing={2} className="mt-2">
+                            <Grid item xs={12} sm={6} >
+                                <FormControl className="w-full">
+                                    <InputLabel id="vacancyType">Tipo de Vaga</InputLabel>
+                                        <Select
+                                            name="tipo"
+                                            labelId="select-tipo"
+                                            label="Tipo de Vaga"
+                                            value={newVacancy.tipo}
+                                            onChange={handleConfidential}
+                                        >
+                                        {VACANCYTYPE?.ARRAY.map((type) => (
+                                            <MenuItem
+                                                key={type?.id}
+                                                value={type?.id}
+                                            >
+                                            {type?.name}
+                                            </MenuItem>
+                                        ))}
+                                        </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} sm={6} >
+                                <FormControl className="w-full">
+                                    <InputLabel id="remuneration">REMUNERAÇÃO</InputLabel>
+                                        <Select
+                                            name="remunerado"
+                                            labelId="select-remuneration"
+                                            label="REMUNERAÇÃO"
+                                            value={newVacancy.remunerado}
+                                            onChange={handleConfidential}
+                                        >
+                                        {REMUNERATION?.ARRAY.map((type) => (
+                                            <MenuItem
+                                                key={type.id}
+                                                value={type.id}
+                                            >
+                                            {type.name}
+                                            </MenuItem>
+                                        ))}
+                                        </Select>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} className="p-2 mt-2">
+                            <Grid item className="justify-center-center" xs={12} sm={6}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <Stack spacing={3}>
+                                        <DesktopDatePicker
+                                            label="Data de Início"
+                                            value={startDate}
+                                            views={["year", "month", "day"]}
+                                            format="DD/MM/YYYY"
+                                            onChange={handleChangeStartDate}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </Stack>
+                                </LocalizationProvider>
+                            </Grid>
+                            <Grid item className="justify-center-center" xs={12} sm={6}>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                    <Stack spacing={3}>
+                                        <DesktopDatePicker
+                                            label="Data de Termino"
+                                            value={finalDate}
+                                            views={["year", "month", "day"]}
+                                            format="DD/MM/YYYY"
+                                            onChange={handleChangeFinalDate}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </Stack>
+                                </LocalizationProvider>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2} justifyContent="start" alignItems="center" className="p-3">
+                            <Grid xs={12}  sm={3} item>
+                                <Checkbox 
+                                    checked={newVacancy.status == 1? true : false}
+                                    onChange={()=>handleConfidential({target:{name:"status", value:newVacancy.status == 1? STATUS.INATIVO : STATUS.ATIVO }})}
+                                    color="primary" 
+                                /> 
+                                <Tooltip 
+                                    arrow 
+                                    title="Por padrão, a vaga sempre vem ativa, mas para facilitar e não precisar excluir vagas, você pode desativar, e ativa-las novamente quando for utilizar."
+                                >
+                                    <Chip 
+                                        label="Vaga Ativa?" 
+                                        onClick={
+                                            ()=>handleConfidential({target:{name: "status", value: newVacancy?.status == 1? 0 : 1 }} )}
+                                        variant={newVacancy?.status == 1? "filled" : "outlined"}
+                                    />
+                                </Tooltip>          
                             
-                            <Checkbox onChange={()=>setConfidentialSalary(!confidentialSalary)} />
-                            <Chip label="Confidencial?" 
-                                variant={confidentialSalary? "filled" : "outlined"}
+                            </Grid>
+                            <Grid xs={12} sm={5} justifyContent={"center"} item>
+                                    <Checkbox 
+                                        checked={newVacancy.confidencial_nome == 1? true : false}
+                                        onChange={()=>handleConfidential({target:{name:"confidencial_nome", value:newVacancy.confidencial_nome == 1? STATUS.INATIVO : STATUS.ATIVO }})}
+                                        color="primary" 
+                                    />
+                                <Tooltip arrow title="Caso seja marcado, o salário será OCULTADO e NÃO PODERÁ ser alterado.">
+                                    <Chip 
+                                        label="Empesa Confidencial?" 
+                                        onClick={
+                                            ()=>handleConfidential({target:{name: "confidencial_nome", value: newVacancy?.confidencial_nome == 1? 0 : 1 }} )}
+                                        variant={newVacancy?.confidencial_nome == 1? "filled" : "outlined"}
+                                    />
+                                </Tooltip>
                                 
-                            />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} className="p-2">
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Descrição"
-                            multiline
-                            rows={8}
-                            className=" w-1/2"
-                            />
+                        <Grid container spacing={2} className="p-2 mt-2">
+                            <Grid item className="justify-center-center" xs={12} sm={6}>
+                                <Button 
+                                    className="bg-green-600 w-full hover:bg-green-500 text-base text-white"
+                                    onClick={()=>console.log(newVacancy)}
+                                > 
+                                        CRIAR VAGA
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12} sm={6} >
+                                <Button 
+                                    className="bg-red-600 w-full hover:bg-red-800 text-base text-white"
+                                    onClick={()=>console.log(startDate)}
+                                > 
+                                    CANCELAR
+                                </Button>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Paper>
-            </Grid>
+                    </Paper>
+                </Grid>
             <LayoutBottom />
         </>
     )
