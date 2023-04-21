21b151ec2c65a8c4b6883365c3228edae97e8db7
Casemiro <lordofgamer@outlook.com>
2023-04-21 13:14:50 -0300

    FEAT: User create screen creation
    created the base separation for the user creation by USER_TYPE

--- web/src/pages/users/userCreate.tsx
+++ web/src/pages/users/userCreate.tsx
@@ -6,8 +6,32 @@ import {
   Button,
   Typography,
   Link,
-} from "@mui/material";
+  Grid,
+  Paper,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material"
+
 import { useRouter } from "next/router";
+import Layout from "../layout";
+import LayoutBottom from "../layoutBottom";
+import { STATUS, USER_TYPE } from "@/config/constants";
+
+interface Users {
+  nome?: string,
+  tipo: number | null,
+  cpf?: string,
+  email?: string, 
+  celular?: string,
+  telefone?: string,
+  status: number,
+  rm?: number | null,
+  cnpj?: string,
+  nome_fantasia?: string,
+  senha: string | null,
+}
 
 export default function Register() {
   const [name, setName] = useState("");
@@ -15,62 +39,126 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
+  
+  const NEW_USER = {
+    senha: null,
+    tipo: null,
+    status: STATUS.INATIVO,
+    email: "",
+    celular: "",
+  }
+  
+  const [userData, setUserData]= useState<Users>(NEW_USER);
 
-  const handleSubmit = async (e) => {
+  const NEW_USER_FUNCIONARIO = {
+    ...NEW_USER,
+    nome: "",
+    cpf: "",
+    tipo: USER_TYPE.FUNCIONARIO
+  };
+
+  const NEW_USER_ALUNO = {
+    ...NEW_USER,
+    nome: "",
+    rm: null,
+    data_nascimento: "",
+    cpf: "",
+    telefone: "",
+    tipo: USER_TYPE.ALUNO
+    
+  }
+
+  const NEW_USER_EMPRESA = {
+    ...NEW_USER,
+    nome_fantasia: "",
+    cnpj: "",
+    telefone:"",
+    tipo: USER_TYPE.EMPRESA
+  }
+
+  const handleChangeUserType = (event:any) =>{
+    if (event.target.value === USER_TYPE.ALUNO){
+      setUserData({...NEW_USER_ALUNO})
+      ;
+    }
+    if (event.target.value === USER_TYPE.EMPRESA){
+      setUserData({...NEW_USER_EMPRESA})
+    }
+    if (event.target.value === USER_TYPE.FUNCIONARIO){
+      setUserData({...NEW_USER_FUNCIONARIO})
+      ;
+    }
+  }
+
+  const handleSubmit = async (e:any) => {
     e.preventDefault();
     try {
       const res = await axios.post("/api/register", { name, email, password });
       if (res.data.success) {
         router.push("/login");
       }
-    } catch (err) {
+    } catch (err:any) {
       setError(err.response.data.message);
     }
   };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" gutterBottom>
-        Register
-      </Typography>
-      {error && (
-        <Typography variant="body1" color="error">
-          {error}
-        </Typography>
-      )}
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Name"
-          fullWidth
-          margin="normal"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          label="Email"
-          fullWidth
-          margin="normal"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          fullWidth
-          margin="normal"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button type="submit" variant="contained" color="primary">
-          Register
-        </Button>
-      </form>
-      <Typography variant="body1">
-        Already have an account?{" "}
-        <Link href="/login" color="secondary">
-          Login
-        </Link>
-      </Typography>
-    </Container>
+    <>
+      <Layout />
+      <Grid container className="mb-24 mt-12 justify-center">
+        <Paper className="bg-white w-3/6 justify-center p-2" >
+          <Grid container spacing={2} >
+            <Grid item xs={12} sm={6} >
+              <FormControl className="w-full">
+                <InputLabel id="remuneration">TIPO DE USUÁRIO</InputLabel>
+                  <Select
+                    name="userType"
+                    labelId="select-userType"
+                    label="Tipo de Usuário"
+                    value={userData.tipo}
+                    onChange={handleChangeUserType}
+                  >
+                    {USER_TYPE?.ARRAY.map((type:{id: number, name:string}) => (
+                      <MenuItem
+                          key={type.id}
+                          value={type?.id}
+                      >
+                        {type.name}
+                      </MenuItem>
+                  ))}
+                  </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid container >
+          { ( userData.tipo === USER_TYPE.ALUNO ) &&
+              <Grid item xs={12}>
+                
+
+
+
+              </Grid>}
+            { (userData.tipo === USER_TYPE.EMPRESA) &&
+              <Grid item xs={12}>
+
+
+
+
+
+              </Grid>}
+            { (userData.tipo === USER_TYPE.FUNCIONARIO) &&
+              <Grid item xs={12}>
+
+
+
+
+
+
+              </Grid>}
+          </Grid>
+        </Paper>
+      </Grid>
+      <LayoutBottom />
+    </>
   );
 }
